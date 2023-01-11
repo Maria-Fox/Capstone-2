2fff06e972d58896888d1431e2c1e2c266dc418f
Maria Lazaro <maria.lazaro.fox@gmail.com>
2023-01-11 12:03:18 -0800

    update db schema

--- DBBreakdown/SQL_Design.sql
+++ DBBreakdown/SQL_Design.sql
@@ -8,21 +8,20 @@ SET XACT_ABORT ON
 BEGIN TRANSACTION QUICKDBD
 
 CREATE TABLE [users] (
-    [id] int  NOT NULL ,
-    [name] text  NOT NULL ,
+    [username] text  NOT NULL ,
     [password] text  NOT NULL ,
     [contact_num] int  NOT NULL ,
     [contact_email] text  NOT NULL ,
     [type_of_user] text  NOT NULL ,
     [bio] text  NOT NULL ,
     CONSTRAINT [PK_users] PRIMARY KEY CLUSTERED (
-        [id] ASC
+        [username] ASC
     )
 )
 
 CREATE TABLE [company_requests] (
     [request_id] int  NOT NULL ,
-    [co_id] int  NOT NULL ,
+    [co_usernane] int  NOT NULL ,
     [project_desc] text  NOT NULL ,
     [timeframe] text  NOT NULL ,
     [searching_for_professional] text  NOT NULL ,
@@ -32,45 +31,48 @@ CREATE TABLE [company_requests] (
 )
 
 CREATE TABLE [likes] (
-    [like_id] int  NOT NULL ,
-    [liker] int  NOT NULL ,
-    [likee] int  NOT NULL ,
+    [id] int  NOT NULL ,
+    [liker_username] int  NOT NULL ,
+    [liked_username] int  NOT NULL ,
     CONSTRAINT [PK_likes] PRIMARY KEY CLUSTERED (
-        [like_id] ASC
+        [id] ASC
     )
 )
 
 CREATE TABLE [messages] (
     [message_id] int  NOT NULL ,
-    [message_from] int  NOT NULL ,
-    [message_to] int  NOT NULL ,
+    [message_from] text  NOT NULL ,
+    [message_to] text  NOT NULL ,
+    [body] text  NOT NULL ,
+    [sent_at] timestamp  NOT NULL ,
+    [read_at] timestamp  NOT NULL ,
     CONSTRAINT [PK_messages] PRIMARY KEY CLUSTERED (
         [message_id] ASC
     )
 )
 
-ALTER TABLE [company_requests] WITH CHECK ADD CONSTRAINT [FK_company_requests_co_id] FOREIGN KEY([co_id])
-REFERENCES [users] ([id])
+ALTER TABLE [company_requests] WITH CHECK ADD CONSTRAINT [FK_company_requests_co_usernane] FOREIGN KEY([co_usernane])
+REFERENCES [users] ([username])
 
-ALTER TABLE [company_requests] CHECK CONSTRAINT [FK_company_requests_co_id]
+ALTER TABLE [company_requests] CHECK CONSTRAINT [FK_company_requests_co_usernane]
 
-ALTER TABLE [likes] WITH CHECK ADD CONSTRAINT [FK_likes_liker] FOREIGN KEY([liker])
-REFERENCES [users] ([id])
+ALTER TABLE [likes] WITH CHECK ADD CONSTRAINT [FK_likes_liker_username] FOREIGN KEY([liker_username])
+REFERENCES [users] ([username])
 
-ALTER TABLE [likes] CHECK CONSTRAINT [FK_likes_liker]
+ALTER TABLE [likes] CHECK CONSTRAINT [FK_likes_liker_username]
 
-ALTER TABLE [likes] WITH CHECK ADD CONSTRAINT [FK_likes_likee] FOREIGN KEY([likee])
-REFERENCES [users] ([id])
+ALTER TABLE [likes] WITH CHECK ADD CONSTRAINT [FK_likes_liked_username] FOREIGN KEY([liked_username])
+REFERENCES [users] ([username])
 
-ALTER TABLE [likes] CHECK CONSTRAINT [FK_likes_likee]
+ALTER TABLE [likes] CHECK CONSTRAINT [FK_likes_liked_username]
 
 ALTER TABLE [messages] WITH CHECK ADD CONSTRAINT [FK_messages_message_from] FOREIGN KEY([message_from])
-REFERENCES [users] ([id])
+REFERENCES [users] ([username])
 
 ALTER TABLE [messages] CHECK CONSTRAINT [FK_messages_message_from]
 
 ALTER TABLE [messages] WITH CHECK ADD CONSTRAINT [FK_messages_message_to] FOREIGN KEY([message_to])
-REFERENCES [users] ([id])
+REFERENCES [users] ([username])
 
 ALTER TABLE [messages] CHECK CONSTRAINT [FK_messages_message_to]
 
